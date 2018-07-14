dcf3780c0aa0a866c96a1bc300dee62443a9c6a1
idleberg <jan@idleberg.com>
2018-07-14 21:37:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# generator-atom-package-webpack
+
+[![npm](https://img.shields.io/npm/l/generator-atom-package-webpack.svg?style=flat-square)](https://www.npmjs.org/package/generator-atom-package-webpack)
+[![npm](https://img.shields.io/npm/v/generator-atom-package-webpack.svg?style=flat-square)](https://www.npmjs.org/package/generator-atom-package-webpack)
+[![David](https://img.shields.io/david/idleberg/generator-atom-package-webpack.svg?style=flat-square)](https://david-dm.org/idleberg/generator-atom-package-webpack)
+
+## Description
+
+A [Yeoman](http://yeoman.io/authoring/user-interactions.html) generator for Atom packages written in JavaScript and compiled using Webpack. This is meant as an experiment, hoping to [reduce activation time](https://discuss.atom.io/t/package-activatetime/56808/14) of the package.
+
+**Features**
+
+- adds any [SPDX](https://spdx.org/licenses/) license
+- adds [CircleCI](https://circleci.com) configuration
+- adds [Travis CI](https://travis-ci.org/) configuration
+- adds [ESLint](https://eslint.org/) configuration
+
+## Installation
+
+Use your preferred [Node](https://nodejs.org/) package manager to install the CLI tool
+
+```sh
+yarn global add generator-atom-package-webpack || npm i generator-atom-package-webpack -g
+```
+
+## Usage
+
+Run the generator and follow its instructions
+
+```sh
+yo atom-package-webpack
+```
+
+*“That's all Folks!”*
+
+## Related
+
+- [generator-atom-package-coffeescript](https://www.npmjs.org/package/generator-atom-package-coffeescript)
+- [generator-atom-package-typescript](https://www.npmjs.org/package/generator-atom-package-typescript)
+
+## License
+
+This work is licensed under the [MIT License](https://opensource.org/licenses/MIT)
---
+++ generators/app/index.js
@@ -0,0 +1,297 @@
+const Generator = require('yeoman-generator');
+const pkg = require('../../package.json');
+
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const slugify = require('@sindresorhus/slugify');
+const spdxLicenseList = require('spdx-license-list/full');
+const updateNotifier = require('update-notifier');
+
+// Create array of license choices
+const spdxCodes = Object.getOwnPropertyNames(spdxLicenseList).sort();
+const licenseChoices = spdxCodes.map(obj =>{
+   const licenses = {};
+   licenses['value'] = obj;
+
+   return licenses;
+})
+
+// Is there a newer version of this generator?
+updateNotifier({ pkg: pkg }).notify();
+
+module.exports = class extends Generator {
+  inquirer() {
+    return this.prompt([
+      {
+        name: 'name',
+        message: 'What do you want to name your package?',
+        default: slugify(this.appname),
+        validate: (str) => {
+          return !str.startsWith('atom-') ? true : 'Your package name shouldn\'t be prefixed with "atom-"' ;
+        }
+      },
+      {
+        name: 'description',
+        message: 'What is your package description?',
+        default: '',
+        validate: (str) => {
+          return str.length > 0 ? true : 'Please provide a short description for your package' ;
+        }
+      },
+      {
+        name: 'author',
+        message: 'What\'s your GitHub username?',
+        store: true,
+        validate: x => x.length > 0 ? true : 'You have to provide a username',
+        when: () => !this.options.org
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose license',
+        default: 'MIT',
+        choices: licenseChoices,
+        store: true
+      },
+      {
+        type: 'checkbox',
+        name: 'features',
+        message: 'Package Features',
+        choices: [
+          {
+            name: 'Grammars',
+            value: 'grammars',
+            checked: false
+          },
+          {
+            name: 'Keymaps',
+            value: 'keymaps',
+            checked: false
+          },
+          {
+            name: 'Menus',
+            value: 'menus',
+            checked: false
+          },
+          {
+            name: 'Snippets',
+            value: 'snippets',
+            checked: false
+          },
+          {
+            name: 'Styles',
+            value: 'styles',
+            checked: false
+          }
+        ]
+      },
+      {
+        type: 'confirm',
+        name: 'activationCmd',
+        message: 'Add activation command?',
+        default: true
+      },
+      {
+        type: 'checkbox',
+        name: 'addConfig',
+        message: 'Add configuration',
+        default: 'MIT',
+        choices: [
+          {
+            name: 'Circle CI',
+            value: 'circleCI',
+            checked: false
+          },
+          {
+            name: 'Travis CI',
+            value: 'travisCI',
+            checked: false
+          }
+        ],
+        store: true
+      },
+      {
+        type: 'list',
+        name: 'eslintConfig',
+        message: 'ESLint style guide',
+        choices: [
+          {
+            name: 'Airbnb',
+            value: 'airbnb',
+          },
+          {
+            name: 'ESLint',
+            value: 'eslint',
+          },
+          {
+            name: 'Google',
+            value: 'google',
+          },
+          {
+            name: 'Idiomatic',
+            value: 'idiomatic',
+          },
+          {
+            name: 'Prettier',
+            value: 'prettier',
+          },
+          {
+            name: 'Standard',
+            value: 'standard',
+          }
+        ],
+        store: true
+      },
+      {
+        type: 'list',
+        name: 'linterHook',
+        message: 'Linter hook',
+        default: 'precommit',
+        choices: [
+          {
+            name: 'precommit',
+            value: 'precommit',
+          },
+          {
+            name: 'prepush',
+            value: 'prepush',
+          },
+          {
+            name: 'prepublishOnly',
+            value: 'prepublishOnly'
+          }
+        ],
+        store: true
+      },
+      {
+        type: 'confirm',
+        name: 'initGit',
+        message: 'Initialize Git repository?',
+        default: fs.existsSync('.git/') ? false : true
+      }
+    ]).then(props => {
+
+      props.licenseURL = spdxLicenseList[props.license].url;
+      props.licenseName = spdxLicenseList[props.license].name;
+      props.licenseText = spdxLicenseList[props.license].licenseText.replace(/\n/g, '\n\n');
+
+      // Copying files
+      props.features.forEach( feature => {
+        mkdirp(feature);
+      });
+
+      if (props.features.indexOf('keymaps') !== -1) {
+        this.fs.copyTpl(
+          this.templatePath('keymaps/keymap.json.ejs'),
+          this.destinationPath(`keymaps/${props.name}.json`),
+          {
+            pkg: props
+          }
+        );
+      }
+
+      if (props.features.indexOf('menus') !== -1) {
+        this.fs.copyTpl(
+          this.templatePath('menus/menu.json.ejs'),
+          this.destinationPath(`menus/${props.name}.json`),
+          {
+            pkg: props
+          }
+        );
+      }
+
+      if (props.features.indexOf('styles') !== -1) {
+        this.fs.copyTpl(
+          this.templatePath('styles/style.less.ejs'),
+          this.destinationPath(`styles/${props.name}.less`),
+          {
+            pkg: props
+          }
+        );
+      }
+
+      mkdirp('src');
+      this.fs.copyTpl(
+        this.templatePath('src/main.ejs'),
+        this.destinationPath(`src/${props.name}.js`),
+        {
+          pkg: props
+        }
+      );
+
+     this.fs.copyTpl(
+        this.templatePath('README.md.ejs'),
+        this.destinationPath('README.md'),
+        {
+          pkg: props
+        }
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('LICENSE.ejs'),
+        this.destinationPath('LICENSE'),
+        {
+          licenseText: props.licenseText
+        }
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('package.json.ejs'),
+        this.destinationPath('package.json'),
+        {
+          pkg: props
+        }
+      );
+
+      if (props.addConfig.indexOf('circleCI') !== -1) {
+        mkdirp('.circleci');
+        this.fs.copyTpl(
+          this.templatePath('_circleci/config.yml'),
+          this.destinationPath('.circleci/config.yml')
+        );
+      }
+
+      if (props.addConfig.indexOf('travisCI') !== -1) {
+        this.fs.copyTpl(
+          this.templatePath('_travis.yml'),
+          this.destinationPath('.travis.yml')
+        );
+      }
+
+      this.fs.copyTpl(
+        this.templatePath('_editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('_gitignore'),
+        this.destinationPath('.gitignore')
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('webpack.config.ejs'),
+        this.destinationPath(`webpack.config.js`),
+        {
+          pkg: props
+        }
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('_eslintrc.ejs'),
+        this.destinationPath(`.eslintrc`),
+        {
+          pkg: props
+        }
+      );
+
+      // Install latest versions of dependencies
+      this.yarnInstall(['babel-core', 'babel-loader', 'webpack', 'webpack-cli']);
+      this.yarnInstall([ 'babel-eslint', 'eslint', `eslint-config-${props.eslintConfig}`, 'eslint-plugin-node', 'husky'], { 'dev': true, ignoreScripts: true });
+
+      // Initialize git repository
+      if (props.initGit) {
+        this.spawnCommandSync('git', ['init']);
+      }
+    });
+  }
+};

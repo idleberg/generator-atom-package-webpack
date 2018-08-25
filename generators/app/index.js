2ad4f5ab6c684c6eb8e34e8d03922e3ed1b30351
idleberg <jan@idleberg.com>
2018-08-25 18:29:04 +0200

    fix Handlebars template extensions

--- generators/app/index.js
+++ generators/app/index.js
@@ -249,7 +249,7 @@ module.exports = class extends Generator {
 
       if (props.features.indexOf('keymaps') !== -1) {
         this.fs.copyTpl(
-          this.templatePath('keymaps/keymap.json.ejs'),
+          this.templatePath('keymaps/keymap.json.hbs'),
           this.destinationPath(`keymaps/${props.name}.json`),
           {
             pkg: props
@@ -259,7 +259,7 @@ module.exports = class extends Generator {
 
       if (props.features.indexOf('menus') !== -1) {
         this.fs.copyTpl(
-          this.templatePath('menus/menu.json.ejs'),
+          this.templatePath('menus/menu.json.hbs'),
           this.destinationPath(`menus/${props.name}.json`),
           {
             pkg: props
@@ -269,7 +269,7 @@ module.exports = class extends Generator {
 
       if (props.features.indexOf('styles') !== -1) {
         this.fs.copyTpl(
-          this.templatePath('styles/style.less.ejs'),
+          this.templatePath('styles/style.less.hbs'),
           this.destinationPath(`styles/${props.name}.less`),
           {
             pkg: props
@@ -279,7 +279,7 @@ module.exports = class extends Generator {
 
       mkdirp('src');
       this.fs.copyTpl(
-        this.templatePath('src/main.ejs'),
+        this.templatePath('src/main.hbs'),
         this.destinationPath(`src/${props.name}.js`),
         {
           pkg: props
@@ -287,7 +287,7 @@ module.exports = class extends Generator {
       );
 
      this.fs.copyTpl(
-        this.templatePath('README.md.ejs'),
+        this.templatePath('README.md.hbs'),
         this.destinationPath('README.md'),
         {
           pkg: props
@@ -295,7 +295,7 @@ module.exports = class extends Generator {
       );
 
       this.fs.copyTpl(
-        this.templatePath('LICENSE.ejs'),
+        this.templatePath('LICENSE.hbs'),
         this.destinationPath('LICENSE'),
         {
           licenseText: props.licenseText
@@ -314,7 +314,7 @@ module.exports = class extends Generator {
       }
 
       this.fs.copyTpl(
-        this.templatePath('package.json.ejs'),
+        this.templatePath('package.json.hbs'),
         this.destinationPath('package.json'),
         {
           pkg: props
@@ -347,7 +347,7 @@ module.exports = class extends Generator {
       );
 
       this.fs.copyTpl(
-        this.templatePath('webpack.config.ejs'),
+        this.templatePath('webpack.config.hbs'),
         this.destinationPath(`webpack.config.js`),
         {
           pkg: props
@@ -355,7 +355,7 @@ module.exports = class extends Generator {
       );
 
       this.fs.copyTpl(
-        this.templatePath('_eslintrc.ejs'),
+        this.templatePath('_eslintrc.hbs'),
         this.destinationPath(`.eslintrc`),
         {
           pkg: props
@@ -363,7 +363,7 @@ module.exports = class extends Generator {
       );
 
       this.fs.copyTpl(
-        this.templatePath('_babelrc.ejs'),
+        this.templatePath('_babelrc.hbs'),
         this.destinationPath(`.babelrc`),
         {
           pkg: props

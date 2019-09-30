78aca369e352bad478646c9cd5dad5e58403c7ce
idleberg <git@idleberg.com>
2019-09-30 21:23:00 +0200

    fix getting github name

--- generators/app/index.js
+++ generators/app/index.js
@@ -94,7 +94,17 @@ module.exports = class extends Generator {
       {
         name: 'author',
         message: 'What\'s your GitHub username?',
-        default: async () => await this.user.github.username(),
+        default: async () => {
+          let username;
+
+          try {
+            username = await this.user.github.username();
+          } catch (error) {
+            username = '';
+          }
+
+          return username;
+        },
         store: true,
         validate: x => x.length > 0 ? true : 'You have to provide a username',
         when: () => !this.options.org
